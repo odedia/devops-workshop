fce9998e2b5a8fa0768f2439e856f1e461b17864
Oded Shopen <odedia@gmail.com>
2020-02-01 15:05:16 +0200

    persistence

---
+++ labs/solutions/06/cloud-native-spring/src/main/java/io/pivotal/domain/City.java
@@ -0,0 +1,36 @@
+package io.pivotal.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name="city")
+public class City implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String county;
+
+    @Column(nullable = false)
+    private String stateCode;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    @Column
+    private String latitude;
+
+    @Column
+    private String longitude;
+
+}
