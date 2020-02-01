5b0cbeeb5ed4d126725e6037a27f8761ff5decb1
Oded Shopen <odedia@gmail.com>
2020-02-01 14:46:42 +0200

    solutions

--- labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
+++ labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
@@ -2,19 +2,13 @@ package io.pivotal.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@RefreshScope
 @RestController
 public class GreetingController {
 
-    @Value("${greeting:Hola}")
-    private String greeting;
-
     @GetMapping("/hello")
     public String hello() {
-        return String.join(" ", greeting, "World!");
+        return "Hello VMware!";
     }
 
 }
